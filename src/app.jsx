c44e6a53d05065647ac43d81b832cc0b0c5f2413
Rudro Khan <rudrokhan.pro@gmail.com>
2022-06-28 11:06:40 +0200

    setup: Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+export function App(props) {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
