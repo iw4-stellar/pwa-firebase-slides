ae8812f404ee095f824159e678fbe51b27efc7f5
Kassim Mohamed <kassim91000@gmail.com>
2022-07-26 23:02:28 +0200

    dashboard done

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
 export function App(props) {
   return (
     <div className="flex justify-center">
-      <h1 className="font-bold text-2xl text-blue-900">
+      <h1 className="title">
         Open Slides PWA       
       </h1>
+      <div className="project-wrapper">
+        <div className="new-project">
+            <a href="index.html">
+                <img src="/src/plus.gif" className="gif"/>
+            </a>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-container">
+            <img src="thumbnail.png" class="thumbnail"/>
+        </div>
+      </div>
+
+
     </div>
   );
 }
